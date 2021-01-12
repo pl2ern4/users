6b0acf83bd0f7b59ea747bc0498f713910899260
prerna <chaturvediprerna03@gmail.com>
2021-01-12 22:30:18 +0530

    added modification

--- src/AboutUser.js
+++ src/AboutUser.js
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
-import { shallowEqual, useSelector, useDispatch } from "react-redux";
+import { shallowEqual, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const AboutUser = ({match:{params:{id}}}) =>{
-    const data = JSON.parse(window.sessionStorage.getItem("data"));
-
-    const dataFromState = useSelector(state => state.userData[id], shallowEqual);
-    const userData = data || dataFromState;
+    const userData = JSON.parse(window.sessionStorage.getItem("data"));
+    
     useEffect(()=>{
         return ()=>window.sessionStorage.removeItem("data")
     },[]);
